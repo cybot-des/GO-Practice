4b09dc6cec763c5e82681f33defe6c0b66e26594
Pooja Deshpande <gobunny2020@gmail.com>
2022-07-10 12:43:31 +0530

    Concept of Methods using Struct

--- Day3/routine.go
+++ Day3/routine.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -23,11 +24,40 @@ func main() {
 	if student.Class == "mca" {
 		fmt.Println(" Hello World!")
 	}
+
+	fmt.Println("Enter Age: ")
+	age, _ := buff.ReadString('\n')
+	age1, err := strconv.Atoi(string(strings.TrimSpace(age)))
+	if err != nil {
+		age1 = 0
+	}
+	student.age = age1
+	fmt.Println("Is age valid? ", student.isValid())
+	student.printStudent()
 }
 
 type Student struct {
 	Name  string
 	Class string
+	age   int
+}
+
+type Teacher struct {
+}
+
+//METHODS
+
+func (s Student) printStudent() {
+	fmt.Println("Student ", s.Name, " class ", s.Class, " Age ", s.age)
+}
+
+func (s Student) isValid() bool {
+	return s.age >= 18
+}
+
+//Overloading like by replacing reciever
+func (t Teacher) isValid() bool {
+	return true
 }
 
 func thankyou() {
