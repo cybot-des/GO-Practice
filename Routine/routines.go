96c10b430a85a35ba6f17c06d46b1f37eed3e33b
Pooja Deshpande <gobunny2020@gmail.com>
2022-07-10 15:43:12 +0530

    updated routines with waitgroup concept

--- Routine/routines.go
+++ Routine/routines.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func main() {
-	go printMessage()
-	go printMessage()
-	go printMessage()
-	go printMessage()
-	go printMessage()
-	go printMessage()
-	fmt.Println("Hello, world!")
-	Hello()
-	time.Sleep(2 * time.Second)
+	//-------EXAMPLE OF ROUTINES USING TIME.SLEEP
+	// go printMessage()
+	// go printMessage()
+	// go printMessage()
+	// go printMessage()
+	// go printMessage()
+	// go printMessage()
+	// fmt.Println("Hello, world!")
+	// Hello()
+	// time.Sleep(2 * time.Second) ---TEMPORARY SOLUTION
+
+	//-------EXAMPLE 1 OF ROUTINES USING WAIT GROUP
+	wg.Add(1)
+	go printMsg()
+	fmt.Println("===STARTED WAITING===")
+	wg.Wait()
+	fmt.Println("===ENDED WAITING===")
+
+	//-------EXAMPLE 2 OF ROUTINES USING WAIT GROUP
+	wg.Add(2)
+	go printMsg()
+	go printMsg()
+	fmt.Println("===STARTED WAITING===")
+	wg.Wait()
+	fmt.Println("===ENDED WAITING===")
+
+	//-------EXAMPLE 3 OF ROUTINES USING WAIT GROUP
+	wg.Add(3)
+	go printMsg()
+	go printMsg()
+	fmt.Println("===STARTED WAITING===")
+	wg.Wait() //Endlessly stuck in waiting loop( deadlock )
+	fmt.Println("===ENDED WAITING===")
+
 }
 
 func Hello() {
@@ -23,6 +51,14 @@ func Hello() {
 }
 
 func printMessage() {
+	for i := 0; i <= 10; i++ {
+		fmt.Println("Printed Index: ", i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func printMsg() {
+	defer wg.Done()
 	for i := 0; i <= 10; i++ {
 		fmt.Println("Printed Index: ", i)
 		time.Sleep(1 * time.Second)
