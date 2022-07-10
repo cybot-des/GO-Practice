11cda6fba9ddab19e55ae381562ee0d5a0ea4b27
Pooja Deshpande <gobunny2020@gmail.com>
2022-07-10 14:58:25 +0530

    Added Go routines

---
+++ Routine/routines.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go printMessage()
+	go printMessage()
+	go printMessage()
+	go printMessage()
+	go printMessage()
+	go printMessage()
+	fmt.Println("Hello, world!")
+	Hello()
+	// time.Sleep(2 * time.Second)
+}
+
+func Hello() {
+	time.Sleep(2 * time.Second)
+	fmt.Println("Hello is called!")
+}
+
+func printMessage() {
+	for i := 0; i <= 10; i++ {
+		fmt.Println("Printed Index: ", i)
+		time.Sleep(1 * time.Second)
+	}
+}
